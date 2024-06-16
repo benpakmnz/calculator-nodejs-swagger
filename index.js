00c1d825c887a4d26b3bb5cb75ff2ee8976c8686
benny <benpakmnz@gmail.com>
2024-06-16 23:59:55 +0300

    fix env variables

--- index.js
+++ index.js
@@ -4,17 +4,15 @@ const http = require("http");
 const app = require("./server");
 
 // Extract server port from BASE_URL
-const [, , serverPort] = process.env.BASE_URL.split(":");
+const { BASE_PORT, PROTOCOL, HOST_NAME, DOCKER_PORT } = process.env;
+const port = DOCKER_PORT || BASE_PORT;
+const fullUrl = `${PROTOCOL}://${HOST_NAME}:${port}`;
 
-if (!serverPort) {
-  throw new Error(
-    "Invalid BASE_URL format. Please provide a valid URL with port"
-  );
+if (!port) {
+  throw new Error("Invalid port format. Please provide a valid port");
 }
 
-http.createServer(app).listen(serverPort, function () {
-  console.log(
-    `Your server is listening on port ${serverPort} (${process.env.BASE_URL})`
-  );
-  console.log(`Swagger-ui is available on ${process.env.BASE_URL}/docs`);
+http.createServer(app).listen(BASE_PORT, function () {
+  console.log(`Your server is listening on port ${port} (${fullUrl})`);
+  console.log(`Swagger-ui is available on ${fullUrl}/docs`);
 });
